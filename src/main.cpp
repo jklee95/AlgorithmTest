82373ab323af5e4d70a7b00426f054623da8b0d2
J. K. Lee <jklee95@users.noreply.github.com>
2021-09-17 03:55:09 +0900

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int A, B, C;
+	int N;
+	cin >> N;
+
+	cout << N << "\n";
+
+	return 0;
+}
